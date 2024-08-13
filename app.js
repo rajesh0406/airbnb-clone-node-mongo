8532ddc3cdc5ec243e6ff19d0359e6e171e25095
JRajesh <rajesh@codingmart.com>
2024-08-13 22:27:49 +0530

    possible fix

--- app.js
+++ app.js
@@ -37,6 +37,15 @@ const server = async () => {
     })
   );
 
+  app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept"
+    );
+    next();
+  });
+
   app.use("/uploads", express.static("uploads"));
 
   app.use(morgan("dev"));
