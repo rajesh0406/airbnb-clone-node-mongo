a31eb5c5e54631b5af58003dde19ca7b8b033a66
JRajesh <rajesh@codingmart.com>
2024-08-13 08:25:07 +0530

    updated app middleware for cors - hope this works

--- app.js
+++ app.js
@@ -25,6 +25,23 @@ const server = async () => {
   app.use(cors({ origin: "*" }));
   app.options("*", cors());
 
+  // Middleware to handle CORS
+  app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader(
+      "Access-Control-Allow-Methods",
+      "GET, POST, PUT, DELETE, OPTIONS"
+    );
+    res.setHeader(
+      "Access-Control-Allow-Headers",
+      "Content-Type, Authorization"
+    );
+    if (req.method === "OPTIONS") {
+      return res.status(204).end();
+    }
+    next();
+  });
+
   app.use("/uploads", express.static("uploads"));
 
   app.use(morgan("dev"));
