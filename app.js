fac03799a7947acdbf3d452fbe7f81e17dc602d0
JRajesh <rajesh@codingmart.com>
2024-08-13 08:35:09 +0530

    removed unused config

--- app.js
+++ app.js
@@ -23,24 +23,6 @@ const server = async () => {
 
   //********* MIDDLEWARE **********/
   app.use(cors({ origin: "*" }));
-  app.options("*", cors());
-
-  // Middleware to handle CORS
-  app.use((req, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    res.setHeader(
-      "Access-Control-Allow-Methods",
-      "GET, POST, PUT, DELETE, OPTIONS"
-    );
-    res.setHeader(
-      "Access-Control-Allow-Headers",
-      "Content-Type, Authorization"
-    );
-    if (req.method === "OPTIONS") {
-      return res.status(204).end();
-    }
-    next();
-  });
 
   app.use("/uploads", express.static("uploads"));
 
